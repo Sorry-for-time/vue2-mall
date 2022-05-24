0bada8fff856697ee4cb0c88d0f4bc451edf83d1
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-05-25 00:36:34 +0800

    CV 完毕(doge)

--- src/pages/Home/MallHome.vue
+++ src/pages/Home/MallHome.vue
@@ -2,18 +2,33 @@
   <div>
     <!-- 三级联动组件 -->
     <TypeNav />
-
     <ListContainer />
-
     <ReCommend />
+    <MallRank />
+    <MallGuessLike />
+    <MallFloor />
+    <MallFloor />
+    <MallBrand />
   </div>
 </template>
 <script>
 import ListContainer from "@/pages/Home/ListContainer/ListContainer.vue";
-import ReCommend from "./ReCommend/ReCommend.vue";
+import ReCommend from "@/pages/Home/ReCommend/ReCommend.vue";
+import MallRank from "@/pages/Home/MallRank/MallRank.vue";
+import MallGuessLike from "@/pages/Home/GuessYouLike/MallGuessLike.vue";
+import MallFloor from "@/pages/Home/MallFloor/MallFloor.vue";
+import MallBrand from "@/pages/Home/MallBrand/MallBrand.vue";
+
 export default {
   name: "MallHome",
-  components: { ListContainer, ReCommend },
+  components: {
+    ListContainer,
+    ReCommend,
+    MallRank,
+    MallGuessLike,
+    MallFloor,
+    MallBrand,
+  },
 };
 </script>
 <style lang="scss" scoped></style>
