3c4e504d83ea554c24a72387ccfd548699f9a313
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-05-25 00:15:40 +0800

    save tmp

--- src/pages/Home/MallHome.vue
+++ src/pages/Home/MallHome.vue
@@ -4,13 +4,16 @@
     <TypeNav />
 
     <ListContainer />
+
+    <ReCommend />
   </div>
 </template>
 <script>
 import ListContainer from "@/pages/Home/ListContainer/ListContainer.vue";
+import ReCommend from "./ReCommend/ReCommend.vue";
 export default {
   name: "MallHome",
-  components: { ListContainer },
+  components: { ListContainer, ReCommend },
 };
 </script>
 <style lang="scss" scoped></style>
